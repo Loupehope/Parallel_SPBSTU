2bc0e1d6fac5238153d05d8dc7b9085a51021378
Vlad Sukhomlinov <dergunchikus@mail.ru>
2022-05-02 05:39:13 -0700

    feat: 4

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -16,255 +16,385 @@ using namespace std;
 
 // Tr.
 
-static const int tile_size = 1024;
+static const int tile_size = 32;
 
-
-void tr_consequent(int* arr1, int array_size)
+void display_board(int* board, int size)
 {
-    for (int i = 1; i < array_size; i++)
-        arr1[0] += arr1[1];
-}
-
-void tr_concurrent_gpu(int* arr1, int array_size)
-{
-    int elementCount = static_cast<int>(array_size);
-    int last_stride = array_size;
-
-    array_view<int, 1> result(array_size, arr1);
-
-    for (int stride = elementCount / 2; stride > 0; stride /= 2)
+    for (int i = 0; i < size + 2; i++)
     {
-        concurrency::extent<1> ext(stride);
-
-        parallel_for_each(ext, [=](index<1> ind) restrict(amp)
-            {
-                result[ind] += result[ind + stride];
-
-                if ((ind[0] == 0) && (last_stride % 2) && (stride > 1))
-                    result[ind] += result[ind + last_stride - 1];
-            });
-
-        last_stride = stride;
-    }
-
-    result.synchronize();
-}
-
-void tr_concurrent_gpu_block(int* arr1, int array_size, int windowWidth)
-{
-    int elementCount = static_cast<int>(array_size);
-    int last_stride = array_size;
-
-    array_view<int, 1> result(array_size, arr1);
-
-    for (int stride = (elementCount / windowWidth); stride > 0; stride /= windowWidth)
-    {
-        concurrency::extent<1> ext(stride);
-
-        parallel_for_each(ext, [=](index<1> ind) restrict(amp)
-            {
-                int sum = 0;
-                for (int i = 0; i < windowWidth; i++)
-                    sum += result[ind + i * stride];
-
-                result[ind] = sum;
-
-                if ((ind[0] == (stride - 1)) && ((last_stride % windowWidth) != 0))
-                {
-                    sum = 0;
-                    for (int i = stride * windowWidth; i < last_stride; i++)
-                        sum += result[ind + i];
-
-                    result[ind] += sum;
-                }
-            });
-
-        last_stride = stride;
-    }
-
-    result.synchronize();
-
-    for (int i = 1; i < last_stride; i++)
-        arr1[0] += arr1[1];
-}
-
-void ras_tr_concurrent_gpu_block(int* arr1, int array_size, int windowWidth)
-{
-    int elementCount = static_cast<int>(array_size);
-    array_view<int, 1> result(elementCount, arr1);
-
-    while (elementCount >= tile_size)
-    {
-        concurrency::extent<1> ext(elementCount);
-
-        parallel_for_each(ext.tile<tile_size>(), [=](tiled_index<tile_size> ind) restrict(amp)
-            {
-                int tid = ind.local[0];
-                tile_static int tileData[tile_size];
-
-                tileData[tid] = result[ind.global[0]];
-
-                ind.barrier.wait();
-
-                for (int stride = 1; stride < tile_size; stride *= 2)
-                {
-                    if (tid % (2 * stride) == 0)
-                        tileData[tid] += tileData[tid + stride];
-
-                    ind.barrier.wait();
-                }
-
-                if (tid == 0)
-                    result[ind.tile[0]] = tileData[0];
-            });
-
-        elementCount /= tile_size;
-    }
-
-    result.synchronize();
-
-    for (int i = 1; i < elementCount; i++)
-        arr1[0] += arr1[1];
-}
-
-void p_ras_tr_concurrent_gpu_block(int* arr1, int array_size, int windowWidth)
-{
-    int elementCount = static_cast<int>(array_size);
-    array_view<int, 1> result(elementCount, arr1);
-    int window = elementCount / windowWidth;
-
-    concurrency::extent<1> ext(window);
-
-    parallel_for_each(ext.tile<tile_size>(), [=](tiled_index<tile_size> ind) restrict(amp)
+        for (int j = 0; j < size + 2; j++)
         {
+            if (board[i * (size + 2) + j] == 1)
+                cout << "|*|";
+            else
+                cout << "|_|";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
-            int tid = ind.local[0];
-            tile_static int tileData[tile_size];
-            int i = ind.global[0];
-            int stride = window * 2;
+void setup_hidden(int* board, int size)
+{
+    int offset_size = size + 2;
+    int full_size = offset_size * offset_size;
 
-            int sum = 0;
-            do
+    for (int i = 0; i < size; i++) {
+        // top
+        board[i + 1] = board[full_size - offset_size - offset_size + 1 + i];
+        // bottom
+        board[full_size - offset_size + 1 + i] = board[offset_size + 1 + i];
+        // left
+        board[(i + 1) * offset_size] = board[(i + 1) * offset_size + size];
+        // right
+        board[(i + 1) * offset_size + offset_size - 1] = board[(i + 1) * offset_size + 1];
+    }
+
+    board[0] = board[full_size - offset_size - 2];
+    board[offset_size - 1] = board[full_size - offset_size - offset_size + 1];
+    board[full_size - offset_size] = board[offset_size + size];
+    board[full_size - 1] = board[offset_size + 1];
+}
+
+int* serial_new_generation(int* board, int size)
+{
+    int* temp_board = new int[(size + 2) * (size + 2)];
+    int offset_size = size + 2;
+    int neighbours;
+
+    for (int i = 1; i < size + 1; i++)
+    {
+        for (int j = 1; j < size + 1; j++)
+        {
+            int idx = (i * offset_size) + j;
+
+            neighbours = board[(i - 1) * offset_size + (j - 1)];
+            neighbours += board[(i - 1) * offset_size + j];
+            neighbours += board[(i - 1) * offset_size + (j + 1)];
+            neighbours += board[i * offset_size + (j - 1)];
+            neighbours += board[i * offset_size + (j + 1)];
+            neighbours += board[(i + 1) * offset_size + (j - 1)];
+            neighbours += board[(i + 1) * offset_size + j];
+            neighbours += board[(i + 1) * offset_size + (j + 1)];
+
+            if (board[idx] == 1)
             {
-                sum += result[i] + result[i + window];
-                i += stride;
-            } while (i < elementCount);
-
-            tileData[tid] = sum;
-
-            ind.barrier.wait();
-
-            for (int stride = (tile_size / 2); stride > 0; stride >>= 1)
-            {
-                if (tid < stride) tileData[tid] += tileData[tid + stride];
-
-                ind.barrier.wait();
+                if (neighbours < 2 || neighbours > 3)
+                    temp_board[idx] = 0;
+                else
+                    temp_board[idx] = 1;
             }
 
-            if (tid == 0)
-                result[ind.tile[0]] = tileData[0];
-        });
+            else
+            {
+                if (neighbours == 3)
+                    temp_board[idx] = 1;
+                else
+                    temp_board[idx] = 0;
+            }
+        }
+    }
 
-    result.synchronize();
+    delete[] board;
+    setup_hidden(temp_board, size);
 
-    for (int i = 1; i < (window / tile_size); i++)
-        arr1[0] += arr1[1];
+    return temp_board;
+}
+
+int* gpu_new_generation(int* board, int size)
+{
+    int offset_size = size + 2;
+    int full_size = offset_size * offset_size;
+    int* temp_board = new int[full_size];
+
+    array_view<const int, 2> input(offset_size, offset_size, board);
+    array_view<int, 2> output(offset_size, offset_size, temp_board);
+    output.discard_data();
+
+    concurrency::extent<2> write(size, size);
+
+    parallel_for_each(write, [=](index<2> idx) restrict(amp)
+        {
+            int neighbours = 0;
+
+            neighbours = input(idx[0], idx[1]);
+            neighbours += input(idx[0], idx[1] + 1);
+            neighbours += input(idx[0], idx[1] + 2);
+            neighbours += input(idx[0] + 1, idx[1]);
+            neighbours += input(idx[0] + 1, idx[1] + 2);
+            neighbours += input(idx[0] + 2, idx[1]);
+            neighbours += input(idx[0] + 2, idx[1] + 1);
+            neighbours += input(idx[0] + 2, idx[1] + 2);
+
+            if (neighbours < 2 || neighbours > 3)
+            {
+                output(idx[0] + 1, idx[1] + 1) = 0;
+            } 
+            else if (neighbours == 3)
+            {
+                output(idx[0] + 1, idx[1] + 1) = 1;
+            }
+            else
+            {
+                output(idx[0] + 1, idx[1] + 1) = input(idx[0] + 1, idx[1] + 1);
+            }
+        }
+    );
+    delete[] board;
+    setup_hidden(temp_board, size);
+
+
+    return temp_board;
+}
+
+int* optimized_gpu_new_generation(int* board, int size)
+{
+    int offset_size = size + 2;
+    int full_size = offset_size * offset_size;
+    int* temp_board = new int[offset_size * offset_size];
+    int* rules = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 };
+
+    array_view<const int, 2> input(offset_size, offset_size, board);
+    array_view<const int, 2> rules_in(2, 9, rules);
+    array_view<int, 2> output(offset_size, offset_size, temp_board);
+    output.discard_data();
+
+    concurrency::extent<2> write(size, size);
+
+    parallel_for_each(write, [=](index<2> idx) restrict(amp)
+        {
+            int neighbours = 0;
+
+            neighbours = input(idx[0], idx[1]);
+            neighbours += input(idx[0], idx[1] + 1);
+            neighbours += input(idx[0], idx[1] + 2);
+            neighbours += input(idx[0] + 1, idx[1]);
+            neighbours += input(idx[0] + 1, idx[1] + 2);
+            neighbours += input(idx[0] + 2, idx[1]);
+            neighbours += input(idx[0] + 2, idx[1] + 1);
+            neighbours += input(idx[0] + 2, idx[1] + 2);
+
+            int alive = input(idx[0] + 1, idx[1] + 1);
+
+            output(idx[0] + 1, idx[1] + 1) = rules_in(alive, neighbours);
+        }
+    );
+
+    delete[] board;
+    setup_hidden(temp_board, size);
+
+
+    return temp_board;
+}
+
+int* const_gpu_new_generation(int* board, int size)
+{
+    int offset_size = size + 2;
+    int full_size = offset_size * offset_size;
+    int* temp_board = new int[offset_size * offset_size];
+    int rules[2][9] = { {0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 0, 0, 0, 0, 0 } };
+
+    array_view<const int, 2> input(offset_size, offset_size, board);
+    array_view<int, 2> output(offset_size, offset_size, temp_board);
+    output.discard_data();
+
+    concurrency::extent<2> write(size, size);
+
+    parallel_for_each(write, [=](index<2> idx) restrict(amp)
+        {
+            int neighbours = 0;
+
+            neighbours = input(idx[0], idx[1]);
+            neighbours += input(idx[0], idx[1] + 1);
+            neighbours += input(idx[0], idx[1] + 2);
+            neighbours += input(idx[0] + 1, idx[1]);
+            neighbours += input(idx[0] + 1, idx[1] + 2);
+            neighbours += input(idx[0] + 2, idx[1]);
+            neighbours += input(idx[0] + 2, idx[1] + 1);
+            neighbours += input(idx[0] + 2, idx[1] + 2);
+
+            int alive = input(idx[0] + 1, idx[1] + 1);
+
+            output(idx[0] + 1, idx[1] + 1) = rules[alive][neighbours];
+        }
+    );
+    delete[] board;
+    setup_hidden(temp_board, size);
+
+
+    return temp_board;
+}
+
+int* tile_gpu_new_generation(int* board, int size)
+{
+    int offset_size = size + 2;
+    int full_size = offset_size * offset_size;
+    int* temp_board = new int[offset_size * offset_size];
+    int rules[2][9] = { {0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 1, 1, 0, 0, 0, 0, 0 } };
+
+    array_view<const int, 2> input(offset_size, offset_size, board);
+    array_view<int, 2> output(offset_size, offset_size, temp_board);
+    output.discard_data();
+
+    concurrency::extent<2> write(size, size);
+
+    parallel_for_each(write.tile<tile_size, tile_size>(), [=](tiled_index<tile_size, tile_size> idx) restrict(amp)
+        {
+            tile_static int tileData[2][9];
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 9; j++) {
+                    tileData[i][j] = rules[i][j];
+                }
+            }
+
+            idx.barrier.wait();
+
+            int neighbours = 0;
+
+            neighbours = input(idx.global[0], idx.global[1]);
+            neighbours += input(idx.global[0], idx.global[1] + 1);
+            neighbours += input(idx.global[0], idx.global[1] + 2);
+            neighbours += input(idx.global[0] + 1, idx.global[1]);
+            neighbours += input(idx.global[0] + 1, idx.global[1] + 2);
+            neighbours += input(idx.global[0] + 2, idx.global[1]);
+            neighbours += input(idx.global[0] + 2, idx.global[1] + 1);
+            neighbours += input(idx.global[0] + 2, idx.global[1] + 2);
+
+            int alive = input(idx.global[0] + 1, idx.global[1] + 1);
+
+            output(idx.global[0] + 1, idx.global[1] + 1) = tileData[alive][neighbours];
+        }
+    );
+    delete[] board;
+    setup_hidden(temp_board, size);
+
+    return temp_board;
 }
 
 // RESULTS
 
-void tr()
-{
-    int* sizes = new int[] { 1048576, 1048576, 2097152, 8388608, 16777216, 33554432 };
 
-    wcout << "\nSERIAL\n\n";
+double tr_1(int* life, int size) {
+    Timer t;
+    int* input = new int[(size + 2) * (size + 2)];
+    memcpy(input, life, (size + 2) * (size + 2));
 
-    for (int i = 0; i < 6; i++)
-    {
-        Timer t;
-
-        int size = sizes[i];
-        int* array_a = new int[size];
-        fill(array_a, array_a + size, 2);
-
+    for (int i = 0; i < 2; i++) {
         t.Start();
-        tr_consequent(array_a, size);
+        for (int i = 0; i < 1024; i++) {
+            input = serial_new_generation(input, size);
+        }
         t.Stop();
         if (i == 0) continue;
-
-        wcout << "Time: " << t.Elapsed() << "\nCount: " << size << '\n';
     }
+    
+    return t.Elapsed();
+}
 
-    wcout << "\nGPU\n\n";
-    for (int i = 0; i < 6; i++)
-    {
-        Timer t;
+double tr2(int* life, int size) {
+    Timer t;
+    int* input = new int[(size + 2) * (size + 2)];
+    memcpy(input, life, (size + 2) * (size + 2));
 
-        int size = sizes[i];
-        int* array_a = new int[size];
-        fill(array_a, array_a + size, 2);
-
+    for (int i = 0; i < 2; i++) {
         t.Start();
-        tr_concurrent_gpu(array_a, size);
+        for (int i = 0; i < 1024; i++) {
+            input = gpu_new_generation(input, size);
+        }
         t.Stop();
         if (i == 0) continue;
-
-        wcout << "Time: " << t.Elapsed() << "\nCount: " << size << '\n';
     }
 
-    wcout << "\nGPU - BLOCK \n\n";
-    for (int i = 0; i < 6; i++)
-    {
-        Timer t;
+    return t.Elapsed();
+}
+double tr3(int* life, int size) {
+    Timer t;
+    int* input = new int[(size + 2) * (size + 2)];
+    memcpy(input, life, (size + 2) * (size + 2));
 
-        int size = sizes[i];
-        int* array_a = new int[size];
-        fill(array_a, array_a + size, 2);
-
+    for (int i = 0; i < 2; i++) {
         t.Start();
-        ras_tr_concurrent_gpu_block(array_a, size, 64);
+        for (int i = 0; i < 1024; i++) {
+            input = optimized_gpu_new_generation(input, size);
+        }
         t.Stop();
         if (i == 0) continue;
-
-        wcout << "Time: " << t.Elapsed() << "\nCount: " << size << '\n';
     }
 
-    wcout << "\nGPU - RAS BLOCK \n\n";
-    for (int i = 0; i < 6; i++)
-    {
-        Timer t;
+    return t.Elapsed();
+}
+double tr4(int* life, int size) {
+    Timer t;
+    int* input = new int[(size + 2) * (size + 2)];
+    memcpy(input, life, (size + 2) * (size + 2));
 
-        int size = sizes[i];
-        int* array_a = new int[size];
-        fill(array_a, array_a + size, 2);
-
+    for (int i = 0; i < 2; i++) {
         t.Start();
-        ras_tr_concurrent_gpu_block(array_a, size, 64);
+        for (int i = 0; i < 1024; i++) {
+            input = const_gpu_new_generation(input, size);
+        }
         t.Stop();
         if (i == 0) continue;
-
-        wcout << "Time: " << t.Elapsed() << "\nCount: " << size << '\n';
     }
 
-    wcout << "\nGPU - P RAS BLOCK \n\n";
-    for (int i = 0; i < 6; i++)
-    {
-        Timer t;
+    return t.Elapsed();
+}
+double tr5(int* life, int size) {
+    Timer t;
+    int* input = new int[(size + 2) * (size + 2)];
+    memcpy(input, life, (size + 2) * (size + 2));
 
-        int size = sizes[i];
-        int* array_a = new int[size];
-        fill(array_a, array_a + size, 2);
-
+    for (int i = 0; i < 2; i++) {
         t.Start();
-        p_ras_tr_concurrent_gpu_block(array_a, size, 128);
+        for (int i = 0; i < 1024; i++) {
+            input = tile_gpu_new_generation(input, size);
+        }
         t.Stop();
         if (i == 0) continue;
-
-        wcout << "Time: " << t.Elapsed() << "\nCount: " << size << '\n';
     }
+
+    return t.Elapsed();
 }
 
 int main()
 {
-    tr();
+    srand(time(0));
+
+    double times[5][5] = {{0, 0, 0, 0, 0}, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }};
+
+    int* sizes = new int[] { 128, 256, 512, 1024, 2048 };
+
+    for (int i = 0; i < 5; i++)
+    {
+        int size = sizes[i];
+        int* life = new int[(size + 2) * (size + 2)];
+        fill(life, life + (size + 2) * (size + 2), 0);
+
+        for (int i = 1; i < size + 1; i++) {
+            for (int j = 1; j < size + 1; j++) {
+                life[i * (size + 2) + j] = rand() % 2;
+            }
+        }
+
+        setup_hidden(life, size);
+
+        times[0][i] = tr_1(life, size);   
+        times[1][i] = tr2(life, size);
+        times[2][i] = tr3(life, size);
+        times[3][i] = tr4(life, size);
+        times[4][i] = tr5(life, size);
+
+        wcout << i << '\n';
+    }
+    wcout << '\n';
+
+    for (int i = 0; i < 5; i++) {
+        wcout << "Mehod: " << i << '\n';
+
+        for (int j = 0; j < 5; j++) {
+            wcout << "Time: " << times[i][j] << " Count: " << sizes[j] << '\n';
+        }
+
+        wcout << '\n';
+    }
 }
